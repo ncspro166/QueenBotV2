04b4b8674200d529c201cc6177e0296dc8dd1981
Priyanshi Kaur <167847176+PriyanshiKaurJi@users.noreply.github.com>
2024-09-30 00:38:36 +0530

    Update g.js

--- scripts/cmds/g.js
+++ scripts/cmds/g.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 
 dotenv.config({ override: true });
 
-const API_KEY = "API_KEY_HERE"; //put key here
+const API_KEY = "AIzaSyByR6RPixARKwvjQF8CHXgTy6_4J60oXn4"; //put key here
 const model = "gemini-1.5-flash-latest";
 const GENAI_DISCOVERY_URL = `https://generativelanguage.googleapis.com/$discovery/rest?version=v1beta&key=${API_KEY}`;
 
