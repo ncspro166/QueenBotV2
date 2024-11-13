ab0107ceb286d4e4d405a6d03a5b1ed0abd1091f
Priyanshi Kaur <sasmitapattnayak104@gmail.com>
2024-11-14 00:31:21 +0530

    Update imgur.js

--- scripts/cmds/imgur.js
+++ scripts/cmds/imgur.js
@@ -3,7 +3,7 @@ const FormData = require('form-data');
 const fs = require('fs');
 const path = require('path');
 
-const IMGUR_CLIENT_ID = "5ba5e80cd3433a7";
+const IMGUR_CLIENT_ID = "YOUR_CILENT_ID"; // Get ID From Imgur developer site
 
 module.exports = {
   config: {
